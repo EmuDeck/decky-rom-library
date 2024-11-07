40a24099479a04839a9d68018b67a0d51251357e
Dragoon Dorise <rodrigosedano@me.com>
2024-11-07 23:40:22 +0100

    wip

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -1,7 +1,8 @@
 import { VFC, useState, useEffect } from "react";
-import { Tabs, Button, Focusable, SteamSpinner, Router } from "decky-frontend-lib";
+import { Tabs, Button, Focusable, SteamSpinner, Router, TextField } from "decky-frontend-lib";
 import { launchApp, getCurrentUserId } from "../common/steamshortcuts";
 import { getTranslateFunc } from "../TranslationsF";
+
 const Games: VFC<{ serverAPI: any }> = ({ serverAPI }) => {
   const [state, setState] = useState<any>({
     games: undefined,
@@ -23,6 +24,8 @@ const Games: VFC<{ serverAPI: any }> = ({ serverAPI }) => {
       toolsPath: undefined,
     },
   });
+
+  const [searchTerm, setSearchTerm] = useState("");
   let { games, tabs, emuDeckConfig } = state;
   const { systemOS } = emuDeckConfig;
   const [currentTab, setCurrentTab] = useState<string>("Tab1");
@@ -30,27 +33,18 @@ const Games: VFC<{ serverAPI: any }> = ({ serverAPI }) => {
 
   const getDataGames = async () => {
     const userId = getCurrentUserId();
-    await serverAPI.callPluginMethod("emudeck", {
-      command: `generateGameLists`,
+    await serverAPI.callPluginMethod("emudeck", { command: `generateGameLists` });
+    serverAPI.callPluginMethod("emudeck", { command: `generateGameListsJson` }).then((response: any) => {
+      const result: any = response.result;
+      const gameList: any = JSON.parse(result);
+      gameList.sort((a: any, b: any) => a.title.localeCompare(b.title));
+      setState({ ...state, games: gameList });
     });
-    serverAPI
-      .callPluginMethod("emudeck", {
-        command: `generateGameListsJson`,
-      })
-      .then((response: any) => {
-        //serverAPI.callPluginMethod("generate_roms_json").then((response: any) => {
-        const result: any = response.result;
-        const gameList: any = JSON.parse(result);
-        gameList.sort((a: any, b: any) => a.title.localeCompare(b.title));
-        setState({ ...state, games: gameList });
-      });
-    //}
   };
 
   const getData = async (update: Boolean) => {
     await serverAPI.callPluginMethod("getSettings", {}).then((response: any) => {
       const result: any = response.result;
-
       const emuDeckConfig: any = JSON.parse(result);
       setState({ ...state, emuDeckConfig });
     });
@@ -73,38 +67,72 @@ const Games: VFC<{ serverAPI: any }> = ({ serverAPI }) => {
   useEffect(() => {
     if (games) {
       const tabs = games.map((item: any) => {
+        const filteredGames = item.games.filter((game: any) =>
+          game.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
         return {
           title: item.title,
           id: item.id,
           content: (
-            <Focusable className="games">
-              {item["games"].map((game: any) => {
-                const random = Math.floor(Math.random() * 10000);
-                return (
-                  <Button
-                    className="game"
-                    key={game.name}
-                    onClick={() => {
-                      launchGame(item.launcher, game.filename, game.name);
-                    }}
-                    onSecondaryActionDescription={"Fix Artwork"}
-                    onSecondaryButton={() => fixArtwork(game.name)}>
-                    <img className="game__img" src={`${game.img}?id=${random}`} alt={game.name} />
-                    <img className="game__bg" src={`${game.img}?id=${random}`} alt={game.name} />
-                  </Button>
-                );
-              })}
-            </Focusable>
+            <>
+              <Focusable className="games__search">
+                <TextField
+                  value={searchTerm}
+                  placeholder="Search games..."
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  style={{ padding: "8px", width: "100%", fontSize: "1rem" }}
+                />
+              </Focusable>
+              <Focusable className="games">
+                {filteredGames.map((game: any) => {
+                  const random = Math.floor(Math.random() * 10000);
+                  return (
+                    <Button
+                      className="game"
+                      key={game.name}
+                      onClick={() => launchGame(item.launcher, game.filename, game.name)}
+                      onSecondaryActionDescription={"Fix Artwork"}
+                      onOKActionDescription="Launch"
+                      onCancelActionDescription="Exit"
+                      onOptionsActionDescription="Add to Favorites"
+                      onMenuActionDescription="Options"
+                      onSecondaryButton={() => fixArtwork(game.name)}
+                      onOptionsButton={() => setFav(game.name)}>
+                      <img
+                        loading="lazy"
+                        className="game__img"
+                        src={`${game.img}?id=${random}`}
+                        alt={game.name.replace(/_/g, " ")}
+                      />
+                      <img
+                        loading="lazy"
+                        className="game__cartridge"
+                        src={`/customimages/emudeck/default/${game.platform}.png?id=${random}`}
+                        alt="Super Nintendo"
+                      />
+                      <img loading="lazy" className="game__bg" src={`${game.img}?id=${random}`} alt={game.name} />
+                      <div className="game__file">
+                        <span>{game.file}</span>
+                      </div>
+                    </Button>
+                  );
+                })}
+              </Focusable>
+            </>
           ),
         };
       });
       setState({ ...state, tabs: tabs });
     }
-  }, [games]);
+  }, [games, searchTerm]);
+
+  const setFav = async (game) => {
+    console.log("HEY");
+  };
 
   const launchGame = (launcher: string, game: string, name: string) => {
     let launcherComplete = launcher.replace(/{file.path}/g, `'${game}'`);
-
     if (emuDeckConfig.systemOS == "nt") {
       launcherComplete = launcherComplete
         .replace(
@@ -120,15 +148,7 @@ const Games: VFC<{ serverAPI: any }> = ({ serverAPI }) => {
         .replace(/\\\\/g, "\\")
         .replace(/\\:"/g, '"Z:');
     }
-
-    launchApp(
-      serverAPI,
-      {
-        name: name,
-        exec: launcherComplete,
-      },
-      systemOS
-    );
+    launchApp(serverAPI, { name, exec: launcherComplete }, systemOS);
   };
 
   const fixArtwork = (game) => {
@@ -137,12 +157,7 @@ const Games: VFC<{ serverAPI: any }> = ({ serverAPI }) => {
   };
 
   return (
-    <div
-      style={{
-        marginTop: "40px",
-        height: "calc(100% - 40px)",
-        background: "#0e141b",
-      }}>
+    <div style={{ marginTop: "40px", height: "calc(100% - 40px)", background: "#0e141b" }}>
       <style>{`
         .games{
           // display: flex;
@@ -164,6 +179,13 @@ const Games: VFC<{ serverAPI: any }> = ({ serverAPI }) => {
           grid-auto-flow: row;
           justify-content: space-between;
         }
+
+
+        .games__search{
+          margin-bottom:12px;
+        }
+
+
         .game{
           //position:relative;
           //width: calc(20% - 15px);
@@ -196,6 +218,8 @@ const Games: VFC<{ serverAPI: any }> = ({ serverAPI }) => {
           filter: brightness(0.8) contrast(1.05) saturate(1);
           transform: scale(1.08)
         }
+
+
         .game__img{
           display: inline-block;
            font-family: Arial, sans-serif;
@@ -204,29 +228,97 @@ const Games: VFC<{ serverAPI: any }> = ({ serverAPI }) => {
            text-align: center;
            width:100%;
            position: relative;
+           z-index:3;
+           min-height: 108px;
+        }
+
+        @keyframes marquee {
+         0%   {
+         transform: translateX(100%);
+         }
+         100% {
+         transform: translateX(-100%);
+         }
+        }
+
+        .game__file{
+          background: rgba(0,0,0,0.6);
+          width: 100%;
+          height: 24px;
+          line-height: 24px;
+          position: absolute;
+          bottom: 0;
+          right: 0;
+          left:0;
+          overflow:hidden;
+          z-index:9;
+          opacity:0;
+          transition: .5s
+        }
+
+        .game.gpfocus .game__file{
+          opacity:1;
+          transition: .5s
+        }
+
+        .game__file span{
+          transform:translateX(100%);
+          animation: marquee 5s linear infinite;
+          display:block;
+          color:#fff
+        }
+
+        .game__title{
+          position: absolute;
+          z-index: 2;
+          top: 0;
+          left: 0;
+          bottom: 0;
+          width: 100%;
+          height: 100%;
+          margin: auto;
+          font-size: 16px;
+          display: block;
+          line-height: 195px;
+          text-transform: uppercase;
+        }
+
+        .game__cartridge{
+          position: absolute;
+          width: 100%;
+          left: 0;
+          top: 0;
         }
 
         .game__img:before{
-          content: '';
-           width: 100%;
-           height: 100%;
-           background-color: #efefef;
-           position: absolute;
-           top: 0;
-           left: 0;
+          content: " ";
+          display: block;
+          position: absolute;
+          top: -10px;
+          left: 0;
+          height: calc(100% + 10px);
+          width: 100%;
+          background-color: rgb(111 111 111);
         }
 
         .game__img:after{
-          content: attr(alt);
-           font-size: 18px;
-           color: rgb(100, 100, 100);
+           line-height: 24px;
+           content: attr(alt);
            display: block;
+           font-size: 20px;
+           font-weight: bold;
            position: absolute;
-           z-index: 2;
-           top: 5px;
+           top: 0;
+           bottom: 0;
+           margin: auto;
+           height: 92px;
            left: 0;
-           width: 100%;
-           height: 100%;
+           width: 96%;
+           text-align: center;
+           letter-spacing: -0.1em;
+           text-transform: uppercase;
+           color: #e7e6e6;
+           font-style: italic
         }
 
         .game__bg{
@@ -252,15 +344,12 @@ const Games: VFC<{ serverAPI: any }> = ({ serverAPI }) => {
         }
       `}</style>
       {!tabs && (
-        <div>
-          <div style={{ textAlign: "center", height: "100vh" }}>
-            <SteamSpinner>
-              <p>{t("loadingGames")}</p>
-            </SteamSpinner>
-          </div>
+        <div style={{ textAlign: "center", height: "100vh" }}>
+          <SteamSpinner>
+            <p>{t("loadingGames")}</p>
+          </SteamSpinner>
         </div>
       )}
-
       {tabs && (
         <Tabs
           activeTab={currentTab}

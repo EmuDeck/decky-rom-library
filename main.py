b90ce5585686e23e7b17698e2ed48946000ffb57
Dragoon Dorise <rodrigosedano@me.com>
2025-01-13 14:59:08 +0100

    nt fixes

--- main.py
+++ main.py
@@ -24,10 +24,12 @@ class Plugin:
     async def getSettings(self):
         pattern = re.compile(r'([A-Za-z_][A-Za-z0-9_]*)=(.*)')
         user_home = os.path.expanduser("~")
+
         if os.name == 'nt':
-            config_file_path = os.path.join(user_home, 'emudeck', 'settings.ps1')
+            config_file_path = os.path.join(user_home, 'AppData', 'Roaming', 'EmuDeck', 'settings.ps1')
         else:
             config_file_path = os.path.join(user_home, 'emudeck', 'settings.sh')
+
         configuration = {}
 
         with open(config_file_path, 'r') as file:
@@ -35,13 +37,16 @@ class Plugin:
                 match = pattern.search(line)
                 if match:
                     variable = match.group(1)
-                    value = match.group(2).strip('"')
-                    configuration[variable] = value
+                    value = match.group(2).strip().strip('"')
+                    expanded_value = os.path.expandvars(value.replace('"', '').replace("'", ""))
+                    configuration[variable] = expanded_value
 
+        # Obtener rama actual del repositorio backend
         if os.name == 'nt':
             bash_command = f"cd {appdata_roaming_path}/EmuDeck/backend/ && git rev-parse --abbrev-ref HEAD"
         else:
             bash_command = "cd $HOME/.config/EmuDeck/backend/ && git rev-parse --abbrev-ref HEAD"
+
         result = subprocess.run(bash_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         configuration["branch"] = result.stdout.strip()
 
